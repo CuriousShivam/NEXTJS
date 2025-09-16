8ae93e3904c34c615834cb2bdb890949534a53d0
CuriousShivam <shivam.sah2023t@gmail.com>
2025-09-16 12:40:56 +0530

    Nodejs runtime removed

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,5 @@ export { auth as middleware } from '@/auth' // <-- path to your auth.ts
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-  runtime: 'nodejs',
+
 };
